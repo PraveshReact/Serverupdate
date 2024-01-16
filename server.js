bf2c3d3b1a8ac18dc5f9e15c682ad9bf9f31e7d1
Pravesh Kumar <140145624+PraveshReact@users.noreply.github.com>
2024-01-16 17:44:06 +0530

    Update server.js

--- server.js
+++ server.js
@@ -18,24 +18,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
-// app.post('/api/insertData', async (req, res) => {
-//     try {
-//         const {id, EnglishBody, EnglishTitle, ItemRank, ItemCover, EventDate, EndDate, EventDescription, ItemDescription } = req.body;
-
-//         //const { Title, Email, Id } = req.body; // Extract Title, Email, and Id from the request body
-
-//         const connection = await dbPool.getConnection();
-//         await connection.query('INSERT INTO Testevents (id, EnglishBody, EnglishTitle, ItemRank, ItemCover, EventDate, EndDate, EventDescription, ItemDescription) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)', [id, EnglishBody, EnglishTitle, ItemRank, ItemCover, EventDate, EndDate, EventDescription, ItemDescription]);
-
-//         //await connection.query('INSERT INTO Sharepoint (Title, Email, Id) VALUES (?, ?, ?)', [Title, Email, Id]);
-//         connection.release();
-
-//         res.status(200).json({ message: 'Data inserted successfully' });
-//     } catch (error) {
-//         console.error('Error inserting data into MySQL:', error);
-//         res.status(500).json({ error: 'Error inserting data' });
-//     }
-// });
 // app.post('/api/tableCreationdata', async (req, res) => {
 //     try {
 //         const itemsToInsert = req.body;
