97d466716650505eea7ec67776c7a555a8ffc1ef
PraveshReact <Praveshkmr825@gmail.com>
2024-02-28 13:17:36 +0530

    KeyTitle

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(bodyParser.json({ limit: '10mb' }));
 app.use(bodyParser.urlencoded({ extended: true, limit: '10mb' }));
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 const dbPool = mysql.createPool({
     host: '185.3.235.202',
@@ -272,6 +272,29 @@ app.get('/api/getDataFilterbase', async (req, res) => {
         const columns = columnsResult[0].map(column => column.Field);
         // Use the column names to build the SELECT query with a WHERE clause for the title
         const selectQuery = `SELECT ${columns.join(', ')} FROM ${table} WHERE Title = ?`;
+        // Execute the SELECT query with the Title parameter
+        const result = await connection.query(selectQuery, [Title]);
+        const data = result[0]; // Assuming the data is in the first element of the result array
+        connection.release();
+        res.status(200).json(data);
+    } catch (error) {
+        console.error('Error retrieving data from MySQL:', error);
+        res.status(500).json({ error: 'Error retrieving data' });
+    }
+});
+app.get('/api/getFilterKeyTitle', async (req, res) => {
+    try {
+        // Extract the table name and title from the query parameters
+        const { table, Title } = req.query;
+        if (!table || !Title) {
+            return res.status(400).json({ error: 'Table name and Title are required' });
+        }
+        const connection = await dbPool.getConnection();
+        // Retrieve column names from the specified table
+        const columnsResult = await connection.query(`SHOW COLUMNS FROM ${table}`);
+        const columns = columnsResult[0].map(column => column.Field);
+        // Use the column names to build the SELECT query with a WHERE clause for the title
+        const selectQuery = `SELECT ${columns.join(', ')} FROM ${table} WHERE KeyTitle = ?`;
         // Execute the SELECT query with the Title parameter
         const result = await connection.query(selectQuery, [Title]);
         const data = result[0]; // Assuming the data is in the first element of the result array
